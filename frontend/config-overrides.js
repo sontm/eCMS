67e51e9368f1ed43b0d3b77d261961abea704a97
SonTran <sontm@users.noreply.github.com>
2019-10-13 01:39:30 +0700

    upgrade react rewired for optimize

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,16 +1,20 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+} = require("customize-cra");
 
-// Primary: #1890FF
-module.exports = function override(config, env) {
-    config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-    config = rewireLess.withLoaderOptions({
-      modifyVars: { 
-          "@layout-body-background": "#f4f4f4",
-          "@layout-header-background": "#189eff",
-          "@layout-footer-background": "#189eff" 
-      },
-      javascriptEnabled: true
-    })(config, env);
-    return config;
-};
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { 
+      "@primary-color": "#1890FF",
+      "@layout-body-background": "#f4f4f4",
+      "@layout-header-background": "#189eff",
+      "@layout-footer-background": "#189eff"
+    }
+  })
+);
